c623677f00af5215fcd06a5a590d9c9969823899
MiaLearnsToCode <meroimia@gmail.com>
2019-08-29 16:21:59 +0100

    crush function

--- js/app.js
+++ js/app.js
@@ -10,13 +10,14 @@ function init() {
   // other variables
   const colors = ['green', 'purple', 'orange', 'blue']
   let inPlay = []
-  let proximity = null
+  let proximity = false
+  let colorCheck = false
 
   // function that checks that you are trying to move a candy only by one cell either vertically or horizontally
   function checkProximity() {
-    const firstCell = parseInt(inPlay[0].getAttribute('data-id'))
-    const secondCell = parseInt(inPlay[1].getAttribute('data-id'))
-    console.log(firstCell, secondCell)
+    console.log('hereone')
+    const firstCell = parseInt(inPlay[0].getAttribute('id'))
+    const secondCell = parseInt(inPlay[1].getAttribute('id'))
     if (firstCell === secondCell + 1 || firstCell === secondCell - 1 || firstCell === secondCell - width || firstCell === secondCell + width) {
       proximity = true
     } else {
@@ -24,9 +25,30 @@ function init() {
     }
   }
 
-  // function that checks if the candy you are moving will crush a row or column 
-  function checkPossibility() {
+  function checkColor() {
+    console.log('here')
+    const color = inPlay[0].classList[1]
+    const index = parseInt(inPlay[1].getAttribute('id'))
     
+    const tt = document.getElementById(`${index - (width * 2)}`)
+    const t = document.getElementById(`${index - width}`)
+    const b = document.getElementById(`${index + width}`)
+    const bb = document.getElementById(`${index + (width * 2)}`)
+    const ll = document.getElementById(`${index - 2}`)
+    const l = document.getElementById(`${index - 1}`)
+    const r = document.getElementById(`${index + 1}`)
+    const rr = document.getElementById(`${index + 2}`)
+    
+    const arrayCheck = [[tt, t], [t,b], [b,bb], [ll, l], [l,r], [r,rr]]
+    let i = 0
+    while (i < arrayCheck.length && colorCheck === false) {
+      if (arrayCheck[i][0] && arrayCheck[i][1] && arrayCheck[i][0].classList[1] === color && arrayCheck[i][1].classList[1] === color) {
+        colorCheck = true
+      } else {
+        colorCheck = false
+      }
+      i++
+    }
   }
 
   // function that swaps two candies
@@ -36,20 +58,50 @@ function init() {
     inPlay[0].classList.remove(first)
     inPlay[0].classList.add(second)
     inPlay[1].classList.remove(second)
-    inPlay[1].classList.add(first)
-    
+    inPlay[1].classList.add(first) 
   }
+
+  // function that loops through the grid and crushes all possible candy
+  function crush() {
+    const cells = document.querySelectorAll('.cell')
+    for (let i = 0; i < cells.length; i++) {
+      const cell = cells[i]
+      const r = cells[i + 1]
+      const rr = cells[i + 2]
+      const l = cells[i - 1]
+      const ll = cells[i - 2]
+      const t = cells[i - width]
+      const tt = cells[i - (width * 2)]
+      const b = cells[i + width]
+      const bb = cells[i + (width * 2)]
+      
+      const arrayCheck = [[tt, t], [t, b], [b, bb], [ll, l], [l, r], [r, rr]]
+      for (let i = 0; i < arrayCheck.length; i++) {
+        if (cell && arrayCheck[i][0] && arrayCheck[i][1] && cell.classList[1] === arrayCheck[i][0].classList[1] && arrayCheck[i][0].classList[1] === arrayCheck[i][1].classList[1]) {
+          cell.classList.remove(`${cell.classList[1]}`)
+          arrayCheck[i][0].classList.remove(`${arrayCheck[i][0].classList[1]}`)
+          arrayCheck[i][1].classList.remove(`${arrayCheck[i][1].classList[1]}`)
+        }
+      }
+    }
+  }
+
+
 
   // function that allows the swap function to run only if the move is allowed
   function play(cell) {
     inPlay.push(cell)
     if (inPlay.length === 2) {
       checkProximity()
-      checkPossibility()
-      if (proximity && possibility) {
+      checkColor()
+      if (proximity && colorCheck) {
         swap()
-      } 
+        proximity = false
+        colorCheck = false
+        crush()
+      }
       inPlay = []
+      
     }
   }
 
@@ -59,14 +111,15 @@ function init() {
       const cell = document.createElement('div')
       cell.classList.add('cell')
       cell.classList.add(`${colors[Math.floor(Math.random() * Math.floor(4))]}`)
-      cell.setAttribute('data-id', i)
+      cell.setAttribute('id', i)
       grid.appendChild(cell)
+      
       cell.addEventListener('click', () => play(cell))
     }
+    crush()
   }
 
   createBoard()
-  
 }
 
 window.addEventListener('DOMContentLoaded', init)
